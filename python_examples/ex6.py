99e6c4723186922358fb69ba7917fc1e9760817c
kianmrf <kianmrf@gmail.com>
2019-09-15 17:12:07 -0400

    Python Examples Updated.

--- python_examples/ex6.py
+++ python_examples/ex6.py
@@ -1,6 +1,3 @@
-number = input("Please Enter an Integer: ")
-comp_mode = input("Press 'f' for factorial of n or 's' for sum of 1-n: ")
-
 def sum(n):
     sum = 0
     for i in range(1,n+1):
@@ -8,11 +5,15 @@ def sum(n):
     print("The sum from 1 to n is: ",str(sum))
 
 def fact(n):
+    if n == 0:
+        return 1
     if n == 1:
         return n
     else:
         return n * fact(n-1)
 
+number = input("Please Enter an Integer: ")
+comp_mode = input("Press 'f' for factorial of n or 's' for sum of 1-n: ")
 
 if comp_mode == 'f':
     print("The factorial of n is: ", str(fact(int(number))))
@@ -20,3 +21,4 @@ elif comp_mode == 's':
     sum(int(number))
 else:
     print("Error! Wrong Format Entered")
+
