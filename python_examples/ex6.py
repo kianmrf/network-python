9e5bac60b27a7ae1ea677058cc22910c59f2c609
kianmrf <kianmrf@gmail.com>
2019-09-15 15:18:50 -0400

    Python Intro Examples Updated.

--- python_examples/ex6.py
+++ python_examples/ex6.py
@@ -1,22 +1,22 @@
 number = input("Please Enter an Integer: ")
 comp_mode = input("Press 'f' for factorial of n or 's' for sum of 1-n:")
 
-if comp_mode == 'f':
-    factorial(number)
-elif comp_mode == 's':
-    sum(number)
-else:
-    print("Error! Wrong Format Entered")
-
 def sum(n):
     sum = 0
-    for i in range(1,int(n)+1):
+    for i in range(1,n+1):
         sum = sum + i
     print("The sum from 1 to n is: ",str(sum))
 
-def factorial(number):
-    if number == 1:
-        return number
+def fact(n):
+    if n == 1:
+        return n
     else:
-        return number*factorial(number-1)
+        return (n * fact(n-1))
 
+
+if comp_mode == 'f':
+    fact(int(number))
+elif comp_mode == 's':
+    sum(int(number))
+else:
+    print("Error! Wrong Format Entered")
