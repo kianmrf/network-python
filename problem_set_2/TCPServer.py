5cace44aeccb598b89d26ef922db7a4f0ee31d41
kianmrf <kianmrf@gmail.com>
2019-10-02 20:18:49 -0400

    Updated sendFile function for TCPServer.py

--- problem_set_2/TCPServer.py
+++ problem_set_2/TCPServer.py
@@ -1,7 +1,7 @@
 #TCPServer.py
 
 from socket import socket, SOCK_STREAM, AF_INET
-#Create a TCP socket 
+#Create a TCP socket
 #Notice the use of SOCK_STREAM for TCP packets
 serverSocket = socket(AF_INET, SOCK_STREAM)
 serverPort=1616
@@ -10,26 +10,31 @@ serverSocket.bind(('', serverPort))
 serverSocket.listen(1)
 print ("Interrupt with CTRL-C")
 while True:
-	try:
-            connectionSocket, addr = serverSocket.accept()
-            print ("Connection from %s port %s" % addr)
-		# Receive the client packet
-            message = connectionSocket.recv(4096).decode()
-            print ("Orignal message from client: ", message)
-		# Send file requested
-	    filename = message.split().partition("/")
-	    sendFile(connectionSocket, filename, 'text/plain')
+    try:
+        connectionSocket, addr = serverSocket.accept()
+        print ("Connection from %s port %s" % addr)
+            # Receive the client packet
+        message = connectionSocket.recv(4096).decode()
+        print ("Orignal message from client: ", message)
+        filename = message.split()[1]
+        # sendFile(connectionSocket, filename, 'text/plain')
+        f = open(filename[1:])
+        outputdata = f.read()
+        for i in range(0, len(outputdata)):
+            connectionSocket.send(outputdata[i])
+        print("Success! File sent!")
+            # Capitalize the message from the client
+        # message = message.upper()
+        # connectionSocket.send(message.encode())
+        connectionSocket.close()
+    except IOError:
+        print("Not foun %s" % filename)
+        sendError(connectionSocket, '404', 'Not found')
+        connectionSocket.close()
+    # except KeyboardInterrupt:
+    #     print ("\nInterrupted by CTRL-C")
+    #     break
 
-		# Capitalize the message from the client
-            # message = message.upper()
-            # connectionSocket.send(message.encode())
-            connectionSocket.close()
-	except KeyboardInterrupt:
-            print ("\nInterrupted by CTRL-C")
-            break
-	except IOError:
-	    print("Not foun %s" % filename)
-	    sendError(connectionSocket, '404', 'Not found')
-	    connectionSocket.close()
 
 serverSocket.close()
+
