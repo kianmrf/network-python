a03fc7380fe9a17dce2bb3ec2bd9f01927a04fb5
kianmrf <kianmrf@gmail.com>
2019-10-03 23:37:06 -0400

    UDPServer & Client python scripts added.

---
+++ problem_set_2/UDPClient.py
@@ -0,0 +1,13 @@
+#UDPClient.py
+
+from socket import socket, SOCK_DGRAM, AF_INET
+
+serverName = 'localhost'
+serverPort = 1617
+clientSocket = socket(AF_INET, SOCK_DGRAM)
+message = raw_input('Input lowercase sentence: ')
+clientSocket.sendto(message, (serverName, serverPort))
+modifiedMessage, addr = clientSocket.recvfrom(2048)
+print modifiedMessage, addr
+clientSocket.close()
+
