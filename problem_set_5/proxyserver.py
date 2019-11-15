fce8c4d9d4cf0ee63a4dc72c0b70c2c44907f7d2
kianmrf <kianmrf@gmail.com>
2019-11-15 14:52:55 -0500

    Added proxyserver.py

---
+++ problem_set_5/proxyserver.py
@@ -0,0 +1,71 @@
+import base64
+import copy
+import thread
+import socket
+import sys
+import os
+import datetime
+import time
+import json
+import threading
+import email.utils as eut
+
+# global variables
+max_connections = 10
+BUFFER_SIZE = 4096
+CACHE_DIR = "./cache"
+BLACKLIST_FILE = ""
+USERNAME_PASSWORD_FILE = "username_password.txt"
+MAX_CACHE_BUFFER = 3
+NO_OF_OCC_FOR_CACHE = 2
+blocked = []
+admins = []
+
+def start_proxy_server():
+
+    # Initialize socket
+    try:
+        proxy_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        proxy_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        proxy_socket.bind(('', proxy_port))
+        proxy_socket.listen(max_connections)
+
+        print "Serving proxy on %s port %s ..." % (
+            str(proxy_socket.getsockname()[0]),
+            str(proxy_socket.getsockname()[1])
+            )
+
+    except Exception as e:
+        print "Error in starting proxy server ..."
+        print e
+        proxy_socket.close()
+        raise SystemExit
+
+
+    # Main server loop
+    while True:
+        try:
+            client_socket, client_addr = proxy_socket.accept()
+            client_data = client_socket.recv(BUFFER_SIZE)
+
+            print
+            print "%s - - [%s] \"%s\"" % (
+                str(client_addr),
+                str(datetime.datetime.now()),
+                client_data.splitlines()[0]
+                )
+
+            thread.start_new_thread(
+                handle_one_request_,
+                (
+                    client_socket,
+                    client_addr,
+                    client_data
+                )
+            )
+
+        except KeyboardInterrupt:
+            client_socket.close()
+            proxy_socket.close()
+            print "\nProxy server shutting down ..."
+            break
